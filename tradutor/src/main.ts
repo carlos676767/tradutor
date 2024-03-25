752d34e6c37cb4365c57790d0a31e98d6c18a793
carlos676767 <carlitobr730@gmail.com>
2024-03-25 08:42:31 -0700

    grid colocado

--- tradutor/src/main.ts
+++ tradutor/src/main.ts
@@ -1,24 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const buscarIdiomas = async ()  => {
+  try {
+    console.log("buscando")
+    const response = await fetch(`https://api.mymemory.translated.net/get?q=ola mundo&langpair=pt|en`,{
+      method: "POST",
+    })
+    
+    const data = await response.json()
+    console.log("dados encontrados");
+    console.log(data)
+  } catch (error) {
+    console.log(error); 
+  }
+}
+buscarIdiomas()
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const idiomaPrincipalInput = document.querySelector("textarea") as HTMLTextAreaElement
+
+
+
