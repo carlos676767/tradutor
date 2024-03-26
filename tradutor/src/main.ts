c2cd4d0c125b5c97b4870931b560b92318ee1b9b
carlos676767 <carlitobr730@gmail.com>
2024-03-26 07:43:05 -0700

    traducao funcionando

--- tradutor/src/main.ts
+++ tradutor/src/main.ts
@@ -1,15 +1,51 @@
-const idiomaPrincipalInput = document.getElementById("idiomaPrincipal") as HTMLTextAreaElement
-const idiomaTraduzido = document.getElementById("idiomaTraduzido") as HTMLTextAreaElement
+
+let idiomaPrincipal:string
+const selecionarIdiomaPrincipal = () => {
+  const selectIdiomaPrincipal = document.getElementById("selectIdiomaPrincipal") as HTMLSelectElement
+  const options = selectIdiomaPrincipal.options
+  selectIdiomaPrincipal.addEventListener("change", () => {
+    if (options[1].selected) {
+      idiomaPrincipal = "en"
+    }else if (options[2].selected) {
+      idiomaPrincipal = "es"
+    }else if (options[3].selected) {
+      idiomaPrincipal = "ru"
+    }else{
+      idiomaPrincipal = "ptbr"
+    }
+  })
+}
 
 
 
-const buscarIdiomas = async (idiomaPrincipal: string, idiomaTraduzido:string)  => {
+let idiomaDestinatario: string
+const funcaoIidomaDestinatrio = () => {
+  const selectIdiomaDeestinatorio = document.getElementById("idiomaParaTraduzir") as HTMLSelectElement
+  const options = selectIdiomaDeestinatorio.options
+  selectIdiomaDeestinatorio.addEventListener("change", () => {
+    if (options[1].selected) {
+      idiomaDestinatario = "en"
+    }else if (options[2].selected) {
+      idiomaDestinatario = "es"
+    }else if (options[3].selected) {
+      idiomaDestinatario = "ru"
+    }else{
+      idiomaDestinatario = "ptbr"
+    }
+  })
+}
+
+
+
+selecionarIdiomaPrincipal();
+funcaoIidomaDestinatrio()
+
+const buscarIdiomas = async ()  => {
   try {
     console.log("buscando")
-    const response = await fetch(`https://api.mymemory.translated.net/get?q=ola mundo&langpair=${idiomaPrincipal}|${idiomaTraduzido}`,{
+    const response = await fetch(`https://api.mymemory.translated.net/get?q=ola mundo&langpair=${idiomaPrincipal}|${idiomaDestinatario}`,{
       method: "POST",
     })
-    
     const data = await response.json()
     console.log("dados encontrados");
     const {responseData } = data
@@ -20,42 +56,8 @@ const buscarIdiomas = async (idiomaPrincipal: string, idiomaTraduzido:string)  =
 }
 
 
-let idiomaDestinarioValor: string = ""
-let armazenarIidomaOriginal: string = ""
-buscarIdiomas(armazenarIidomaOriginal, idiomaDestinarioValor)
 
-
-const idiomaOriginal = () => {
-  const selectIdiomaPrincipal = document.getElementById("selectIdiomaPrincipal") as HTMLSelectElement;
-  selectIdiomaPrincipal.addEventListener("change", () => {
-    const options = selectIdiomaPrincipal.options;
-    if (options[1].selected) {
-     armazenarIidomaOriginal = "en";
-    }else if (options[2].selected) {
-      armazenarIidomaOriginal = "es"; 
-    }else if (options[3].selected) {
-      armazenarIidomaOriginal = "pt-BR";
-    }else if (options[4].selected) {
-      armazenarIidomaOriginal = "ru"
-    }
-  });
-};
-idiomaOriginal()
-
-const idiomaDestinatrio = () => {
-  const idiomaParaTraduzir =  document.getElementById("idiomaParaTraduzir") as HTMLSelectElement
-  const options = idiomaParaTraduzir.options;
-  idiomaParaTraduzir.addEventListener("click", () => {
-    if (options[1].selected) {
-      armazenarIidomaOriginal = "en";
-     }else if (options[2].selected) {
-       armazenarIidomaOriginal = "es"; 
-     }else if (options[3].selected) {
-       armazenarIidomaOriginal = "pt-BR";
-     }else if (options[4].selected) {
-       armazenarIidomaOriginal = "ru"
-     }
-  })
-  
-}
-idiomaDestinatrio()
+const button = document.querySelector("button") as HTMLButtonElement
+button.addEventListener("click", () => {
+  buscarIdiomas()
+})
