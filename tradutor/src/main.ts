74c62395e4749dc09474de74f7181e0f05b025f2
carlos676767 <carlitobr730@gmail.com>
2024-03-25 18:36:22 -0700

    ..

--- tradutor/src/main.ts
+++ tradutor/src/main.ts
@@ -1,21 +1,41 @@
-const buscarIdiomas = async ()  => {
+const idiomaPrincipalInput = document.getElementById("idiomaPrincipal") as HTMLTextAreaElement
+const idiomaTraduzido = document.getElementById("idiomaTraduzido") as HTMLTextAreaElement
+const idiomaParaTraduzir =  document.getElementById("idiomaParaTraduzir") as HTMLSelectElement
+
+
+let armazenarIidomaOriginal: string = ""
+const idiomaOriginal = () => {
+  const selectIdiomaPrincipal = document.getElementById("selectIdiomaPrincipal") as HTMLSelectElement;
+  selectIdiomaPrincipal.addEventListener("change", () => {
+    const options = selectIdiomaPrincipal.options;
+    if (options[1].selected) {
+      alert("gay");
+    }
+  });
+};
+
+const buscarIdiomas = async (idiomaPrincipal: string, idiomaTraduzido:string)  => {
   try {
     console.log("buscando")
-    const response = await fetch(`https://api.mymemory.translated.net/get?q=ola mundo&langpair=pt|en`,{
+    const response = await fetch(`https://api.mymemory.translated.net/get?q=ola mundo&langpair=${idiomaPrincipal}|${idiomaTraduzido}`,{
       method: "POST",
     })
     
     const data = await response.json()
     console.log("dados encontrados");
-    console.log(data)
+    const {responseData } = data
+    console.log(responseData.translatedText);
   } catch (error) {
     console.log(error); 
   }
 }
-buscarIdiomas()
 
 
-const idiomaPrincipalInput = document.querySelector("textarea") as HTMLTextAreaElement
+buscarIdiomas("en", "pt")
+
+
+
+
 
 
 
