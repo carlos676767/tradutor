38531c95b949d9939339daf3a372214ba1dee1d1
carlos676767 <carlitobr730@gmail.com>
2024-03-27 07:03:49 -0700

    inpu seperado por funcao

--- tradutor/src/main.ts
+++ tradutor/src/main.ts
@@ -3,9 +3,9 @@
 
 
 let idiomaPrincipal:string
+const selectIdiomaPrincipal = document.getElementById("selectIdiomaPrincipal") as HTMLSelectElement
+const options = selectIdiomaPrincipal.options
 const selecionarIdiomaPrincipal = () => {
-  const selectIdiomaPrincipal = document.getElementById("selectIdiomaPrincipal") as HTMLSelectElement
-  const options = selectIdiomaPrincipal.options
   selectIdiomaPrincipal.addEventListener("change", () => {
     if (options[1].selected) {
       idiomaPrincipal = "en"
@@ -22,15 +22,16 @@ const selecionarIdiomaPrincipal = () => {
 
 
 let idiomaDestinatario: string
+const selectIdiomaDeestinatorio = document.getElementById("idiomaParaTraduzir") as HTMLSelectElement
+const options2 = selectIdiomaDeestinatorio.options
+
 const funcaoIidomaDestinatrio = () => {
-  const selectIdiomaDeestinatorio = document.getElementById("idiomaParaTraduzir") as HTMLSelectElement
-  const options = selectIdiomaDeestinatorio.options
   selectIdiomaDeestinatorio.addEventListener("change", () => {
-    if (options[1].selected) {
+    if (options2[1].selected) {
       idiomaDestinatario = "en"
-    }else if (options[2].selected) {
+    }else if (options2[2].selected) {
       idiomaDestinatario = "es"
-    }else if (options[3].selected) {
+    }else if (options2[3].selected) {
       idiomaDestinatario = "ru"
     }else{
       idiomaDestinatario = "pt-br"
@@ -43,11 +44,15 @@ const funcaoIidomaDestinatrio = () => {
 selecionarIdiomaPrincipal();
 funcaoIidomaDestinatrio();
 
-const buscarIdiomas = async ()  => {
+const pegarValorDigitado = () => {
   const textNeareaIdiomaPrincipal = document.getElementById("idiomaPrincipal") as HTMLTextAreaElement
+  return textNeareaIdiomaPrincipal.value
+}
+
+const buscarIdiomas = async ()  => {
   try {
     console.log("buscando")
-    const response = await fetch(`https://api.mymemory.translated.net/get?q=${textNeareaIdiomaPrincipal.value}&langpair=${idiomaPrincipal}|${idiomaDestinatario}`,{
+    const response = await fetch(`https://api.mymemory.translated.net/get?q=${pegarValorDigitado()}&langpair=${idiomaPrincipal}|${idiomaDestinatario}`,{
       method: "POST",
     })
     const data = await response.json()
@@ -58,6 +63,7 @@ const buscarIdiomas = async ()  => {
   } catch (error) {
     console.log(error); 
   }
+  
 }
 
 
